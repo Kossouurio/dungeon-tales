fea567b5f10d9b9976e1eeef6a77985990bc0a1a
mysticalspread <87622522+mysticalspread@users.noreply.github.com>
2023-04-27 11:06:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ AOS.init();
 $(document).ready(function(){
     $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2300,
         fade: true,
         cssEase: 'linear',
         arrows: false,
